fbc82bef5c60cb04123d58ed2cb0fd1846315e0f
Fabian Sieper <sieper.fabian@outlook.com>
2023-07-25 00:45:05 +0200

    Fixed initialization of the project

--- bin/init_project.py
+++ bin/init_project.py
@@ -5,9 +5,9 @@ CONFIG_FILE_NAME="config.json"
 
 # Config file keys
 NOTION_API_KEY_FIELD = "notion_api_key"
-NOTION_DATABASE_URL_FIELD = "notion_database_url"
+NOTION_DATABASE_ID_FIELD = "notion_database_id"
 NOTION_DATABASE_NAME_FIELD = "notion_database_name"
-REQUIRED_CONFIG_FIELDS = [NOTION_API_KEY_FIELD, NOTION_DATABASE_URL_FIELD, NOTION_DATABASE_NAME_FIELD]
+REQUIRED_CONFIG_FIELDS = [NOTION_API_KEY_FIELD, NOTION_DATABASE_ID_FIELD, NOTION_DATABASE_NAME_FIELD]
 
 def get_user_info():
 
@@ -19,9 +19,11 @@ def get_user_info():
 
             notion_api_key = input("Enter the Notion API key: ")
             database_id = input("Enter the database id, from which you would like to read time information: ")
+            database_name = input("Enter the database name: ")
 
             user_data[NOTION_API_KEY_FIELD] = notion_api_key
-            user_data[NOTION_DATABASE_URL_FIELD] = database_id
+            user_data[NOTION_DATABASE_ID_FIELD] = database_id
+            user_data[NOTION_DATABASE_NAME_FIELD] = database_name
 
             json.dump(user_data, file)
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from bin.init_project import get_user_info
-from bin.init_project import NOTION_API_KEY_FIELD, NOTION_DATABASE_URL_FIELD, NOTION_DATABASE_NAME_FIELD
+from bin.init_project import NOTION_API_KEY_FIELD, NOTION_DATABASE_ID_FIELD, NOTION_DATABASE_NAME_FIELD
 
 from bin.notion_connector import *
 from bin.generic_modules.notion.notion_comparator import *
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     user_info = get_user_info()
     notion = NotionConnector(
         user_info[NOTION_API_KEY_FIELD],
-        user_info[NOTION_DATABASE_URL_FIELD],
+        user_info[NOTION_DATABASE_ID_FIELD],
         user_info[NOTION_DATABASE_NAME_FIELD]
         )
 
